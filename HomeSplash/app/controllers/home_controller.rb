9732089813108ba8fd0fac4ccb0e77244a7b35dd
Jolanda Walter <jolandawalter@gmail.com>
2015-03-02 09:50:32 -0600

    added Bootstrap

--- HomeSplash/app/controllers/home_controller.rb
+++ HomeSplash/app/controllers/home_controller.rb
@@ -2,18 +2,18 @@ class HomeController < ApplicationController
   def index
     right_now = Time.now #now the time is stored it is done ONCE
 
-    @current_time = right_now.to_formatted_s(:short) #we are using right now
+    @current_time = right_now.stamp_like("Jan 1, 2000 ~ 3:00 am") #we are using right now
 
     #Adjust message by time of day
     @greet_message = case right_now.hour #we are using right now AGAIN
     when 5..11
-      "Good Morning, ya'll"
+      "Good Morning, ya\'ll <i class='fa fa-sun-o'></i> x".html_safe
     when 12..15
-      "Happy Afternoon"
+      "Happy Afternoon <i class='fa fa-car'></i> z".html_safe
     when 16..19
-      "Good Evening"
+      "Good Evening X"
     when 20..23, 0..4
-      "Good Night, get some sleep"
+      "Good Night, get some <i class='fa fa-bed'></i> Z".html_safe
     end
   end
 end
